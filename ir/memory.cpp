66ac1a76924bb729754c5777f9763f51e94dfdd2
Juneyoung Lee <aqjune@gmail.com>
2021-02-01 00:32:55 +0900

    allow nonlocal only

--- ir/memory.cpp
+++ ir/memory.cpp
@@ -1118,7 +1118,7 @@ expr Memory::PtrInput::operator==(const PtrInput &rhs) const {
 }
 
 expr Memory::mkFnRet(const char *name, const vector<PtrInput> &ptr_inputs) {
-  bool has_local = escaped_local_blks.numMayAlias(true);
+  bool has_local = false; //escaped_local_blks.numMayAlias(true);
 
   unsigned bits_bid = has_local ? bits_for_bid : Pointer::bitsShortBid();
   expr var
